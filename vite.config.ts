8d6652a07431b59ab9cc5c19653ff8314e87189b
MaruthiCharan2403 <yssmc24@gmail.com>
2025-10-06 19:36:50 +0530

    dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        parserOpts: {
+          plugins: ['decorators-legacy']
+        }
+      }
+    }),
+    tailwindcss()
+  ],
   build: {
     rollupOptions: {
       input: {
